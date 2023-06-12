8a8f9a63c547b70ff0edec76ca0a4ec30eb675fe
3eonah <3eonah.lee@gmail.com>
2023-06-12 18:18:33 +0900

    EX06 array complete

---
+++ JAVASCRIPT-TUTORIAL/array.js
@@ -0,0 +1,15 @@
+//스프레드 문법
+var mbti=['ENFP','ISTJ','INTP'];
+var newmbti=[...mbti, 'CUTE','SEXY'];
+console.log(mbti);
+console.log(newmbti);
+
+//unshift
+var newnewmbti=mbti.unshift('ESFJ');
+console.log(newnewmbti);    // ['ESFJ','ENFP','ISTJ','INTP']
+
+//join
+console.log(mbti.join());
+console.log(mbti.join('-'));
+
+
