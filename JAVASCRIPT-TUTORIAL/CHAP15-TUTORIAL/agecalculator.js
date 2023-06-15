a29a72a336c316cba846b4eff1be17149aa1e552
3eonah <3eonah.lee@gmail.com>
2023-06-15 18:42:40 +0900

    chap.15 실습중

---
+++ JAVASCRIPT-TUTORIAL/CHAP15-TUTORIAL/agecalculator.js
@@ -0,0 +1,32 @@
+let el = document.getElementById('birthday');
+let elResult = document.getElementById('result');
+let elSubmit = document.getElementById('submit');
+
+const today = new Date(); //오늘 날짜
+
+function calculateAge() {
+
+    let birthDate = new Date(el.value);
+    let age;
+
+    if (today.getMonth() > birthDate.getMonth() ||
+        (today.getMonth() == birthDate.getMonth() &&
+            today.getDate() >= birthDate.getDate())
+    ) {
+        //생일이 지난 상황이라면
+        age = today.getFullYear() - birthDate.getFullYear();
+
+    }
+    else {
+        age = today.getFullYear() - birthDate.getFullYear() - 1;
+    }
+
+    elResult.innerText = `${age} 세 입니다.`;
+}
+
+elSubmit.addEventListener('click', calculateAge);
+
+
+
+
+
